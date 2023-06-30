7b0c917f00251d7951a1c90e7303d722610244b9
jina <pja9362@g.skku.edu>
2023-06-30 17:23:31 +0900

    feat(home): tab 구현

--- app/src/main/java/com/example/madcampproj1/MainActivity.kt
+++ app/src/main/java/com/example/madcampproj1/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.madcampproj1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.madcampproj1.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val fragmentList = listOf(tab1Fragment(), tab2Fragment(), tab3Fragment())
+
+        val adapter = FragmentAdapter(this)
+        adapter.fragmentList = fragmentList
+
+        binding.viewPager.adapter = adapter
+
+        // Tab Title 설정
+        val tabTitles = listOf("tab1", "tab2", "tab3")
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) {tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
+
     }
 }
