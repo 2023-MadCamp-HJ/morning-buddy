70170c30ae41e6b8bcc2d38d9acf4876d0abb146
skykhs3 <skykhs3@naver.com>
2023-07-05 13:42:08 +0900

    Design(activity_tab1_add): 디자인 초안

--- app/src/main/java/com/example/madcampproj1/tab1/Tab1AddActivity.kt
+++ app/src/main/java/com/example/madcampproj1/tab1/Tab1AddActivity.kt
@@ -10,11 +10,13 @@ import androidx.core.content.ContentResolverCompat
 import com.example.madcampproj1.R
 import com.example.madcampproj1.databinding.ActivityTab1AddBinding
 import com.example.madcampproj1.databinding.ActivityTab1EditBinding
+import kotlin.random.Random
 
 class Tab1AddActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTab1AddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityTab1AddBinding.inflate(layoutInflater)
         val view = binding.root
@@ -28,7 +30,139 @@ class Tab1AddActivity : AppCompatActivity() {
         binding.tab1CancleButton.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+        binding.tab1DummyButton.setOnClickListener {
+            binding.tab1NewName.setText(generateRandomName())
+            binding.tab1NewPhone.setText(generateRandomPhoneNumber())
+        }
+    }
 
+    fun generateRandomName(): String {
+        val firstNames = listOf(
+            "김",
+            "이",
+            "박",
+            "최",
+            "정",
+            "강",
+            "조",
+            "윤",
+            "장",
+            "임",
+        )
+        val lastNames = listOf(
+            "지훈",
+            "서연",
+            "민준",
+            "하은",
+            "지우",
+            "도윤",
+            "하린",
+            "유준",
+            "시아",
+            "준우",
+            "유진",
+            "은우",
+            "수아",
+            "지민",
+            "지유",
+            "예진",
+            "지현",
+            "예준",
+            "하율",
+            "서준",
+            "유나",
+            "시우",
+            "하윤",
+            "지아",
+            "준서",
+            "시현",
+            "서아",
+            "승준",
+            "예서",
+            "민서",
+            "하연",
+            "지원",
+            "하준",
+            "도현",
+            "소율",
+            "지율",
+            "예은",
+            "윤서",
+            "주원",
+            "수민",
+            "서율",
+            "은서",
+            "지안",
+            "윤우",
+            "시윤",
+            "은유",
+            "유빈",
+            "연우",
+            "민재",
+            "하림",
+            // 추가된 이름들
+            "유림",
+            "세준",
+            "민아",
+            "태윤",
+            "수현",
+            "예린",
+            "은채",
+            "태현",
+            "승희",
+            "수진",
+            "혜원",
+            "주현",
+            "재희",
+            "소연",
+            "우진",
+            "현주",
+            "예담",
+            "주하",
+            "서진",
+            "지한",
+            "윤재",
+            "민희",
+            "은비",
+            "수빈",
+            "진아",
+            "하영",
+            "성훈",
+            "하경",
+            "영서",
+            "재윤",
+            "재원",
+            "윤지",
+            "하진",
+            "은재",
+            "소민",
+            "재민",
+            "유리",
+            "영민",
+            "지성",
+            "지환",
+            "주은",
+            "도은",
+            "세아",
+            "우리",
+            "예지",
+            "서우",
+            "승민",
+            "지수"
+        )
+
+
+        val firstName = firstNames.random()
+        val lastName = lastNames.random()
+
+        return "$firstName$lastName"
+    }
+
+    fun generateRandomPhoneNumber(): String {
+        val number1 = Random.nextInt(0, 10000).toString().padStart(4, '0')
+        val number2 = Random.nextInt(0, 10000).toString().padStart(4, '0')
+
+        return "010-$number1-$number2"
     }
 
     fun addContact(context: Context, name: String, phoneNumber: String) {
