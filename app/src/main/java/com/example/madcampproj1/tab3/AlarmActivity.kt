b5ea96ddaa47d05f6227cd10fd77b5930708f858
jina <pja9362@g.skku.edu>
2023-07-05 10:35:41 +0900

    feat(tab3): 알람 해제 3번 실패시 연락처 첫번째 사람에게 메시지

--- app/src/main/java/com/example/madcampproj1/tab3/AlarmActivity.kt
+++ app/src/main/java/com/example/madcampproj1/tab3/AlarmActivity.kt
@@ -6,8 +6,10 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.os.Looper
+import android.provider.ContactsContract
 import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -21,10 +23,18 @@ import java.util.*
 class AlarmActivity : AppCompatActivity() {
     private var isDismissed = false
     private var isTemporaryExit = false
+    private var alarmCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
+
+        // alarmCount 초기화
+        val alarmId = intent.getIntExtra("ALARM_ID", -1)
+        if (alarmId != -1) {
+            val sharedPreferences = getSharedPreferences("alarms", Context.MODE_PRIVATE)
+            alarmCount = sharedPreferences.getInt("alarmCount$alarmId", 0)
+        }
 
         val dismissButton = findViewById<Button>(R.id.dismissButton)
         dismissButton.setOnClickListener {
@@ -32,7 +42,6 @@ class AlarmActivity : AppCompatActivity() {
             MyBroadcastReceiver.ringtone?.stop()
 
             // 해당 알람의 isEnabled 속성을 false로 설정합니다.
-            val alarmId = intent.getIntExtra("ALARM_ID", -1)
             if (alarmId != -1) {
                 val sharedPreferences = getSharedPreferences("alarms", Context.MODE_PRIVATE)
                 val alarmsJson = sharedPreferences.getString("alarms", "[]")
@@ -74,36 +83,172 @@ class AlarmActivity : AppCompatActivity() {
             // 알람이 임시 종료되었을 경우 알람을 다시 활성화합니다.
             val alarmId = intent.getIntExtra("ALARM_ID", -1)
             if (alarmId != -1) {
-                val sharedPreferences = getSharedPreferences("alarms", Context.MODE_PRIVATE)
-                val alarmsJson = sharedPreferences.getString("alarms", "[]")
-                val alarmsArray = JSONArray(alarmsJson)
-                for (i in 0 until alarmsArray.length()) {
-                    val alarmJson = alarmsArray.getJSONObject(i)
-                    if (alarmJson.getInt("id") == alarmId) {
-                        alarmJson.put("isEnabled", true)
-                        break
+                if (alarmCount < 2) {
+                    val sharedPreferences = getSharedPreferences("alarms", Context.MODE_PRIVATE)
+                    val alarmsJson = sharedPreferences.getString("alarms", "[]")
+                    val alarmsArray = JSONArray(alarmsJson)
+                    for (i in 0 until alarmsArray.length()) {
+                        val alarmJson = alarmsArray.getJSONObject(i)
+                        if (alarmJson.getInt("id") == alarmId) {
+                            alarmJson.put("isEnabled", true)
+                            break
+                        }
+                    }
+                    sharedPreferences.edit().putString("alarms", alarmsArray.toString()).apply()
+
+                    // 1분 후에 알람이 다시 울리도록 설정합니다.
+                    val calendar = Calendar.getInstance().apply {
+                        add(Calendar.MINUTE, 1)
+                    }
+                    val intent = Intent(this, MyBroadcastReceiver::class.java).apply {
+                        putExtra("ALARM_ID", alarmId)
+                    }
+                    val pendingIntent =
+                        PendingIntent.getBroadcast(this, alarmId, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+                    val alarmManager =
+                        getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                    alarmManager.setExact(
+                        AlarmManager.RTC_WAKEUP,
+                        calendar.timeInMillis,
+                        pendingIntent
+                    )
+
+                    alarmCount++
+
+                    // alarmCount 저장
+                    sharedPreferences.edit().putInt("alarmCount$alarmId", alarmCount).apply()
+                } else {
+                    // 해당 알람의 isEnabled 속성을 false로 설정합니다.
+                    val sharedPreferences = getSharedPreferences("alarms", Context.MODE_PRIVATE)
+                    val alarmsJson = sharedPreferences.getString("alarms", "[]")
+                    val alarmsArray = JSONArray(alarmsJson)
+                    for (i in 0 until alarmsArray.length()) {
+                        val alarmJson = alarmsArray.getJSONObject(i)
+                        if (alarmJson.getInt("id") == alarmId) {
+                            alarmJson.put("isEnabled", false)
+                            break
+                        }
+                    }
+                    sharedPreferences.edit().putString("alarms", alarmsArray.toString()).apply()
+
+                    // 연락처 앱과 연동하여 가장 첫번째 사람에게 문자 메시지를 보냅니다.
+                    val cursor = contentResolver.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null,
+                        null,
+                        null,
+                        null
+                    )
+                    if (cursor != null && cursor.moveToFirst()) {
+                        val columnIndex =
+                            cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                        if (columnIndex != -1) {
+                            val phoneNumber = cursor.getString(columnIndex)
+                            cursor.close()
+
+                            val smsIntent = Intent(Intent.ACTION_SENDTO).apply {
+                                data = Uri.parse("smsto:$phoneNumber")
+                                putExtra("sms_body", "모닝콜 부탁해요!")
+                            }
+                            startActivity(smsIntent)
+                        } else {
+                            cursor.close()
+                        }
                     }
                 }
-                sharedPreferences.edit().putString("alarms", alarmsArray.toString()).apply()
-
-                // 1분 후에 알람이 다시 울리도록 설정합니다.
-                val calendar = Calendar.getInstance().apply {
-                    add(Calendar.MINUTE, 1)
-                }
-                val intent = Intent(this, MyBroadcastReceiver::class.java).apply {
-                    putExtra("ALARM_ID", alarmId)
-                }
-                val pendingIntent =
-                    PendingIntent.getBroadcast(this, alarmId, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-                val alarmManager =
-                    getSystemService(Context.ALARM_SERVICE) as AlarmManager
-                alarmManager.setExact(
-                    AlarmManager.RTC_WAKEUP,
-                    calendar.timeInMillis,
-                    pendingIntent
-                )
             }
         }
     }
 }
+
+
+//class AlarmActivity : AppCompatActivity() {
+//    private var isDismissed = false
+//    private var isTemporaryExit = false
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_alarm)
+//
+//        val dismissButton = findViewById<Button>(R.id.dismissButton)
+//        dismissButton.setOnClickListener {
+//            // 알람 소리를 끕니다.
+//            MyBroadcastReceiver.ringtone?.stop()
+//
+//            // 해당 알람의 isEnabled 속성을 false로 설정합니다.
+//            val alarmId = intent.getIntExtra("ALARM_ID", -1)
+//            if (alarmId != -1) {
+//                val sharedPreferences = getSharedPreferences("alarms", Context.MODE_PRIVATE)
+//                val alarmsJson = sharedPreferences.getString("alarms", "[]")
+//                val alarmsArray = JSONArray(alarmsJson)
+//                for (i in 0 until alarmsArray.length()) {
+//                    val alarmJson = alarmsArray.getJSONObject(i)
+//                    if (alarmJson.getInt("id") == alarmId) {
+//                        alarmJson.put("isEnabled", false)
+//                        break
+//                    }
+//                }
+//                sharedPreferences.edit().putString("alarms", alarmsArray.toString()).apply()
+//            }
+//
+//            // 앱이 임시 종료됩니다.
+//            isDismissed = true
+//            isTemporaryExit = true
+//            finish()
+//        }
+//
+//        // 30초 후에 코드 실행 (알람 지속 시간 30초)
+//        val handler = HandlerCompat.createAsync(Looper.getMainLooper())
+//        handler.postDelayed({
+//            if (!isDismissed) {
+//                // 알람 해제되지 않음
+//                // 앱 임시 종료
+//                isTemporaryExit = true
+//                finish()
+//            }
+//        }, 30 * 1000)
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        if (!isDismissed && isTemporaryExit) {
+//            // 알람 소리를 끕니다.
+//            MyBroadcastReceiver.ringtone?.stop()
+//
+//            // 알람이 임시 종료되었을 경우 알람을 다시 활성화합니다.
+//            val alarmId = intent.getIntExtra("ALARM_ID", -1)
+//            if (alarmId != -1) {
+//                val sharedPreferences = getSharedPreferences("alarms", Context.MODE_PRIVATE)
+//                val alarmsJson = sharedPreferences.getString("alarms", "[]")
+//                val alarmsArray = JSONArray(alarmsJson)
+//                for (i in 0 until alarmsArray.length()) {
+//                    val alarmJson = alarmsArray.getJSONObject(i)
+//                    if (alarmJson.getInt("id") == alarmId) {
+//                        alarmJson.put("isEnabled", true)
+//                        break
+//                    }
+//                }
+//                sharedPreferences.edit().putString("alarms", alarmsArray.toString()).apply()
+//
+//                // 1분 후에 알람이 다시 울리도록 설정합니다.
+//                val calendar = Calendar.getInstance().apply {
+//                    add(Calendar.MINUTE, 1)
+//                }
+//                val intent = Intent(this, MyBroadcastReceiver::class.java).apply {
+//                    putExtra("ALARM_ID", alarmId)
+//                }
+//                val pendingIntent =
+//                    PendingIntent.getBroadcast(this, alarmId, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+//
+//                val alarmManager =
+//                    getSystemService(Context.ALARM_SERVICE) as AlarmManager
+//                alarmManager.setExact(
+//                    AlarmManager.RTC_WAKEUP,
+//                    calendar.timeInMillis,
+//                    pendingIntent
+//                )
+//            }
+//        }
+//    }
+//}
